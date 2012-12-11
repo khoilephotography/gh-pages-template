c1c36b33ec5daf9a9224e8902fcd89896b44b580
4nakin <ldtuan87@yahoo.com>
2012-12-11 09:22:11 -0800

    Create images_loader.js

---
+++ images_loader.js
@@ -0,0 +1,6 @@
+jQuery( 'div.b-sidebar a')
+    .click(function() {
+        $("#fotorama").empty().append('<img src="http://farm5.static.flickr.com/4148/4967746406_427d761e2e_b.jpg"/>');
+        $("#fotorama").prepend('<img src="http://farm5.static.flickr.com/4148/4967746406_427d761e2e_b.jpg"/>');
+        return false;
+    });​
