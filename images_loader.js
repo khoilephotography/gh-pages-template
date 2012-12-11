db2ff29405b8ae42afd8770c4a3647f12050242d
4nakin <ldtuan87@yahoo.com>
2012-12-12 03:12:20 +0700

    Update images_loader.js

--- images_loader.js
+++ images_loader.js
@@ -4,5 +4,5 @@ $( 'div.b-sidebar a')
         $("#fotorama").empty().append('<img src="http://farm5.static.flickr.com/4148/4967746406_427d761e2e_b.jpg"/>');
         $("#fotorama").prepend('<img src="http://farm5.static.flickr.com/4148/4967746406_427d761e2e_b.jpg"/>');
         return false;
-    })​
+        })​
 });
